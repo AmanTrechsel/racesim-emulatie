426a2b14bdb9795bcd5c481512edc453a698d949
AmanTrechse <30940054+Voltseon@users.noreply.github.com>
2024-05-28 21:07:43 +0200

    Added comments

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+  // Serialized fields
   [SerializeField]
   private Vector2 platformSize;
   [SerializeField]
@@ -15,103 +16,165 @@ public class Controller : MonoBehaviour
   [SerializeField]
   private MeshFilter inversePlatformMeshFilter;
   [SerializeField]
-  [Range(-45, 45f)]
+  [Range(-90, 90)]
   private float pitch, roll;
   [SerializeField]
   private float cylinderBaseY;
 
+  // Private variables
   private MeshFilter platformMeshFilter;
   private float platformPitch, platformRoll;
   private float xCenter, yCenter;
+  private Vector3 normal;
 
+  // Start is called before the first frame update
   private void Start()
   {
+    // Get the mesh filter of the platform
     platformMeshFilter = platform.GetComponent<MeshFilter>();
+
+    // Calculate the center of the platform
     xCenter = platformSize.x / 2;
     yCenter = platformSize.y / 2;
   }
 
+  // Update is called once per frame
   private void Update()
   {
+    // Handle the platform and cylinders
     HandlePlatform();
     HandleCylinders();
+
+    // Update the UI
     UpdateUI();
   }
 
+  // This method is responsible for drawing the gizmos
+  private void OnDrawGizmos()
+  {
+    // Draw the normal of the platform
+    Gizmos.color = Color.green;
+    Gizmos.DrawRay(platform.position, normal * 10);
+  }
+
+  // This method is responsible for setting the pitch of the platform
   public void SetPitch(float pitch)
   {
     this.pitch = pitch;
   }
 
+  // This method is responsible for setting the roll of the platform
   public void SetRoll(float roll)
   {
     this.roll = roll;
   }
 
+  // This method is responsible for positioning the platform
   private void HandlePlatform()
   {
+    // Set the position of the platform
     platform.position = GetAveragePosition();
+
+    // Set the mesh of the platform
     Vector3[] vertices = GetVertices();
     platformMeshFilter.mesh = CalculateMesh(vertices);
     inversePlatformMeshFilter.mesh = platformMeshFilter.mesh;
+
+    // Calculate the angles of the platform
     CalculateAngles(vertices);
   }
 
+  // This method is responsible for positioning the cylinders
   private void HandleCylinders()
   {
+    // Iterate through each cylinder
     for (int i = 0; i < 4; i++) {
-      float yPos = cylinderBaseY;
+      // Base position of the cylinder
+      float yPos = 0;
+
+      // Add Pitch and Roll
       yPos += roll / 45 * xCenter * ((i % 2 == 0) ? -1 : 1);
       yPos += pitch / 45 * yCenter * ((i < 2) ? -1 : 1);
+
+      // X & Z position of the cylinder
       float xPos = (i % 2 == 0) ? xCenter : -xCenter;
       float zPos = (i < 2) ? yCenter : -yCenter;
+
+      // Add the base position of the cylinder
+      yPos += cylinderBaseY;
+
+      // Set the position of the cylinder
       cylinders[i].position = new Vector3(xPos, yPos, zPos);
     }
   }
 
+  // This method is responsible for updating the UI
   private void UpdateUI()
   {
     uiHandler.UpdatePitch(platformPitch);
     uiHandler.UpdateRoll(platformRoll);
   }
 
+  // This method calculates the average position of the cylinders
   private Vector3 GetAveragePosition()
   {
+    // Initialize the average position
     Vector3 averagePosition = Vector3.zero;
+
+    // Iterate through each cylinder
     foreach (Transform cylinder in cylinders)
     {
       averagePosition += cylinder.position;
     }
+
+    // Calculate the average position
     averagePosition /= cylinders.Count;
+
+    // Return the average position
     return averagePosition;
   }
 
+  // This method calculates the mesh of the platform
   private Mesh CalculateMesh(Vector3[] vertices)
   {
+    // Create a new mesh
     Mesh mesh = new Mesh();
+
+    // Set the vertices and triangles of the mesh
     mesh.vertices = vertices;
     mesh.triangles = new int[] { 0, 1, 2, 3, 4, 5 };
+
+    // Calculate the normals of the mesh
+    mesh.RecalculateNormals();
+
+    // Return the mesh
     return mesh;
   }
 
+  // This method calculates the angles of the platform
   private void CalculateAngles(Vector3[] vertices)
   {
+    // Calculate the normals of the platform
     Vector3 normal1 = Vector3.Cross(vertices[1] - vertices[0], vertices[2] - vertices[0]);
     Vector3 normal2 = Vector3.Cross(vertices[3] - vertices[0], vertices[5] - vertices[0]);
 
-    Vector3 combinedNormal = normal1 + normal2;
-    combinedNormal.Normalize();
+    // Combine the normals
+    normal = normal1 + normal2;
+    normal.Normalize();
 
+    // If the dot product of the normals is greater than 0, reverse the combined normal
     float dot = Vector3.Dot(normal1, normal2);
     if (dot > 0)
     {
-      combinedNormal *= -1;
+      normal *= -1;
     }
 
-    platformPitch = 90 - Vector3.Angle(combinedNormal, Vector3.forward);
-    platformRoll = 90 - Vector3.Angle(combinedNormal, Vector3.right);
+    // Calculate the platform pitch and roll
+    platformPitch = 90 - Vector3.Angle(normal, Vector3.forward * normal.magnitude);
+    platformRoll = 90 - Vector3.Angle(normal, Vector3.right * normal.magnitude);
   }
 
+  // This method returns the vertices of the platform
   private Vector3[] GetVertices()
   {
     return new Vector3[] { cylinders[1].position, cylinders[0].position, cylinders[2].position, cylinders[1].position, cylinders[2].position, cylinders[3].position };
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class UIHandler : MonoBehaviour
 {
+  // Serialized fields
   [SerializeField]
   private TextMeshProUGUI pitchText, rollText;
   [SerializeField]
@@ -13,27 +14,33 @@ public class UIHandler : MonoBehaviour
   [SerializeField]
   private Slider pitchSlider, rollSlider;
 
+  // Update is called once per frame
   private void Update()
   {
+    // Update the pitch and roll slider labels
     SetPitchSlider();
     SetRollSlider();
   }
 
+  // This method is responsible for updating the pitch label
   public void UpdatePitch(float pitch)
   {
     pitchText.text = "Pitch: " + pitch.ToString("F2");
   }
 
+  // This method is responsible for updating the roll label
   public void UpdateRoll(float roll)
   {
     rollText.text = "Roll: " + roll.ToString("F2");
   }
 
+  // This method is responsible for setting the label of the pitch slider
   public void SetPitchSlider()
   {
     sliderPitchLabel.text = "Pitch: " + pitchSlider.value.ToString("F2");
   }
 
+  // This method is responsible for setting the label of the roll slider
   public void SetRollSlider()
   {
     sliderRollLabel.text = "Roll: " + rollSlider.value.ToString("F2");
